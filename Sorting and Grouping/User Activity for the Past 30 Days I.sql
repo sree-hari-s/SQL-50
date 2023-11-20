721c48d012418bdfd5de4b7e00cdcc823e451044
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-20 19:45:16 +0530

    3 sorting problems completed

---
+++ Sorting and Grouping/User Activity for the Past 30 Days I.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select 
+    activity_date as day,
+    count(distinct user_id) as active_users
+from Activity
+where activity_date > DATEADD(DAY, -30, '2019-07-27') 
+and activity_date <= '2019-07-27'
+group by activity_date
